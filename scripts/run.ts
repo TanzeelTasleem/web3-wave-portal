8599147d7153296d61d1456540b8b334b99ef2a6
Tanzeel Tasleem <tanzeeltasleem648@gmail.com>
2022-11-17 21:23:34 +0500

    deploy on local blockchain

--- scripts/run.ts
+++ scripts/run.ts
@@ -3,11 +3,13 @@ import { WavePortal__factory, WavePortal } from "../typechain-types";
 
 const main = async () => {
     const [owner, randomPerson] = await ethers.getSigners()
+    const accountBalance = await owner.getBalance();
     const waveContractFactory: WavePortal__factory = await ethers.getContractFactory("WavePortal");
     const waveContract: WavePortal = await waveContractFactory.deploy();
     await waveContract.deployed();
     console.log("Contract deployed to:", waveContract.address);
     console.log("owner of the contract is", owner.address);
+    console.log("Account balance: ", accountBalance.toString());
 
     const waveTxn = await waveContract.wave()
     console.log("waved by owner", owner.address);
@@ -19,7 +21,7 @@ const main = async () => {
     await waveTxn2.wait();
     console.log("waved by random address", randomPerson.address);
     // You can also see that wallet address that waved equaled to the address that wave.
- 
+
     await waveContract.getTotalWaves()
 
 };
